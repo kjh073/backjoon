abba707f28618b4056140ed68f340cec4e486ac8
Joohee KIm <82260747+kjh073@users.noreply.github.com>
2024-04-13 01:37:58 +0900

    [level 1] Title: 음양 더하기, Time: 0.20 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == False:
+            answer += (absolutes[i] * -1)
+        else:
+            answer += absolutes[i]
+    return answer
